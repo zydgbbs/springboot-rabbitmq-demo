234ff396423b6957d2e84ef17f94f75728293dc1
zydgbbs <admin@zydgbbs.com>
2019-12-19 21:53:48 +0800

    init

---
+++ src/test/java/com/zydgbbs/springbootrabbitmqdemo/SpringbootRabbitmqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zydgbbs.springbootrabbitmqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
